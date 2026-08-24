d9067480f76bb5bb2fa08855681e5b9a8154cb5e
mcleanka <mcleankasambala@gmail.com>
2026-08-24 13:10:22 +0200

    Sort hotspot eligible villages by name

--- location/schema.py
+++ location/schema.py
@@ -133,7 +133,7 @@ class Query(graphene.ObjectType):
             hotspot = Hotspot.get_queryset(None, info.context.user).filter(
                 uuid=hotspot_uuid, validity_to__isnull=True
             ).first()
-        return get_hotspot_eligible_villages(micro_catchment, hotspot).order_by("code")
+        return get_hotspot_eligible_villages(micro_catchment, hotspot).order_by("name")
 
     def resolve_hotspots(self, info, **kwargs):
         if info.context.user.is_anonymous:
